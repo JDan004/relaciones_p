08e2c8b1b14019ed3fa44c66eb477db147321ff6
Jhordan <jhordanf614@gmail.com>
2025-03-30 18:50:44 -0500

    relación 1 a 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Models\Phone;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('prueba', function () {
+    /*  User::create([
+        'name' => 'Jhordan Flores',
+        'email' => 'jhordan@newsoftec.com',
+        'password' => bcrypt('12345678')
+    ]); */
+
+    /*  Phone::create([
+        'number' => '978654321',
+        'user_id' => 4
+    ]);
+
+    return "Creado"; */
+
+    /* $user = User::find(4);
+    return $user->phone; */
+
+    
 });
